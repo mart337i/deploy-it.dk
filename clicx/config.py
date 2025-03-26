fd9aef081e9b09ccb4ea28b0e92df2c589e4e78b
martin <mart337i@gmail.com>
2025-03-26 20:17:43 +0100

    [REM] Remove database unused code

--- clicx/config.py
+++ clicx/config.py
@@ -1,11 +1,8 @@
-from pathlib import Path
 from dotenv import load_dotenv
-from typing import List
-import configparser
 import glob
 import os
 import rich
-from clicx import templates_dir, addons
+from clicx import addons
 from clicx.utils.jinja import render
 
 
@@ -24,48 +21,6 @@ class Configuration:
         self.config = {}
         self.env_variables = {}
         
-        self.verify_config_setup()
-
-    def verify_config_setup(self):
-        """
-        Will create nessesay config file, if not found
-        """
-        def _exist(path: Path):
-            if path.exists():
-                return True
-            else:
-                return False
-                
-        default_config_path = ".config/clicx/clicx.conf"
-        config_path: Path = Path(Path(__file__).parent.parent, default_config_path)
-        
-        if not _exist(config_path):
-            os.makedirs(config_path.parent, exist_ok=True)
-            self.create_config(output_path=config_path)
-            return False
-        
-        self.load_config(config_file=config_path)
-        return True
-
-    def load_config(self, config_file):
-        parser = configparser.ConfigParser()
-        parser.read(config_file)
-
-        for option in parser.defaults():
-            if 'DEFAULT' not in self.config:
-                self.config['DEFAULT'] = {}
-            self.config['DEFAULT'][option] = parser.get('DEFAULT', option)
-        
-        for section in parser.sections():
-            if section not in self.config:
-                self.config[section] = {}
-        
-            for option in parser.options(section):
-                if section == 'DEFAULT' and option in parser.defaults():
-                    continue
-                value = parser.get(section, option)
-                self.config[section][option] = value
-
     def create_config(self,output_path):
         template = render(template_name="clicx.conf.jinja")
         try:
