fc6e59782ec12cf279a7c1c55d4a9fdf1df4d100
egeskov <mart337i@gmail.com>
2025-04-02 08:26:32 +0200

    [ADD] Add comments

--- clicx/__init__.py
+++ clicx/__init__.py
@@ -3,8 +3,10 @@ import os
 from pkgutil import extend_path
 from pathlib import Path
 
+# NOTE: 
+# This makes it posible to do `from clicx import x` instead of haveing to do the abselute path.
+# This is also called a namespace package.
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 __path__ = extend_path(__path__, __name__)
 
 __version__ = '1.0.0'
@@ -15,8 +17,12 @@ templates_dir: Path = Path(Path(__file__).parent, 'templates')
 addons: Path = Path(Path(__file__).parent.parent, 'addons')
 project_root : Path = Path(Path(__file__).parent.parent)
 
-if addons.exists():
-    sys.path.insert(0, str(addons))
+
+# NOTE: 
+# This makes it so instead of hveing to write `from addons.proxmox.models.proxmox import proxmox`
+# you can do `from proxmox.models.proxmox import proxmox`. 
+# This is done to impove modularization 
+sys.path.insert(0, str(addons))
 
 from . import utils
 from . import config
